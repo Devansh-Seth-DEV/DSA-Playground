1e9308341991965b741f1724a4149b37cb628f83
Devansh Seth <129330424+Devansh-Seth-DEV@users.noreply.github.com>
2025-07-16 23:16:08 +0530

    Create Range_Min_Max_Queries.cpp

---
+++ Problems/GFG/Range Query And Update/Range_Min_Max_Queries.cpp
@@ -0,0 +1,212 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+template <typename T = int, typename U = int>
+class SegmentTree {
+public:
+    using CombineFunction = function<T(const T&, const T&, const int)>;
+    using TransformFunction = function<T(const U&)>;
+    
+private:
+    vector<T> segTree;
+    U* arrayPtr = nullptr;
+    int levels = 0;
+    
+    long long arraySize = 0, maxUsedIndex = 0;
+    T identity;
+    CombineFunction combine;
+    TransformFunction transform;
+                            
+    void init(U* array, long long size) {
+        arrayPtr = array;
+        arraySize = size;
+        segTree.resize(arraySize << 2, identity);
+        build(0, 0, arraySize - 1, levels);
+    }
+    
+    void init(vector<U>& array) {
+        init(array.data(), array.size());
+    }
+
+    T build(const long long i,
+            const long long low, const long long high,
+            const int level) {
+        maxUsedIndex = max(maxUsedIndex, i);
+        
+        if (low==high) {
+            segTree[i] = transform(arrayPtr[low]);
+            return segTree[i];
+        }
+        
+        long long left = (i<<1) + 1;
+        long long right = left+1;
+        long long mid = low + ((high-low)>>1);
+        
+        T leftVal = build(left, low, mid, level-1);
+        T rightVal = build(right, mid+1, high, level-1);
+        
+        segTree[i] = combine(leftVal, rightVal, level);
+        return segTree[i];
+    }
+    
+    void updateSegTree(const long long idx, const U newVal,
+                    const long long i,
+                    const long long low, const long long high,
+                    const int level) {
+        if (low == high) {
+            segTree[i] = transform(newVal);
+            arrayPtr[idx] = newVal;
+            return;
+        }
+        
+        long long left = (i<<1) + 1;
+        long long right = left+1;
+        long long mid = low + ((high-low)>>1);
+        
+        if (idx <= mid)
+            updateSegTree(idx, newVal, left, low, mid, level-1);
+        else
+            updateSegTree(idx, newVal, right, mid+1, high, level-1);
+        
+        T leftVal = segTree[left];
+        T rightVal = segTree[right];
+        segTree[i] = combine(leftVal, rightVal, level);
+    }
+    
+    T queryRange(const long long l, const long long r,
+                const long long i,
+                const long long low, const long long high,
+                const int level) const {
+        if (high < l || r < low)
+            return identity;
+        
+        if (l <= low && high <= r)
+            return segTree[i];
+        
+        long long left = (i<<1) + 1;
+        long long right = left+1;
+        long long mid = low + ((high-low)>>1);
+        
+        T leftVal = queryRange(l, r, left, low, mid, level-1);
+        T rightVal = queryRange(l, r, right, mid+1, high, level-1);
+        T val = combine(leftVal, rightVal, level);
+        return val;
+    }
+    
+    
+public:
+    
+    SegmentTree(U array[], const long long N,
+                CombineFunction op = [](const T& a, const T& b,
+                                    const int level) -> T {
+                    return a+b;
+                },
+                T idEle = 0,
+                TransformFunction tf = [](const U& a) -> T {
+                    return a;
+                }) :    levels((int) ceil(log2(N))),
+                        combine(op),
+                        identity(idEle),
+                        transform(tf)
+    { init(array, N); }
+    
+    SegmentTree(vector<U>& array,
+                CombineFunction op = [](const T& a, const T& b,
+                                    const int level) -> T {
+                    return a+b;
+                },
+                T idEle = 0,
+                TransformFunction tf = [](const U& a) -> T {
+                    return a;
+                }) :    levels((int) ceil(log2(array.size()))),
+                        combine(op),
+                        identity(idEle),
+                        transform(tf)
+    { init(array); }
+    
+    void update(const long long i, const U newVal) {
+        updateSegTree(i, newVal, 0, 0, arraySize-1, levels);
+    }
+    
+    int getLevels() const {
+        return levels;
+    }
+    
+    long long getMaxUsedIndex() const {
+        return maxUsedIndex;
+    }
+    
+    const T& getNode(const long long i) const {
+        return segTree[i];
+    }
+    
+    T getIdentity() const {
+        return identity;
+    }
+    
+    bool isIdentity(const T& idEle,
+                    function<bool(const T&, const T&)> isEqual = [](const T& a,
+                                                                    const T& b) -> bool {
+                        return a == b;
+                    }) const {
+        return isEqual(idEle, identity);
+    }
+    
+    T query(const long long i, const long long j) const {
+        return queryRange(i, j, 0, 0, arraySize-1, levels);
+    }
+    
+    friend ostream& operator<<(ostream& os, const SegmentTree<T,U>& st) {
+        for(long long i=0; i<=st.maxUsedIndex; i++)
+            os << st.segTree[i] << " ";
+        return os;
+    }
+};
+
+int main() {
+    int n, q;
+    cin >> n >> q;
+    
+    vector<int> arr(n);
+    for(auto& a: arr) cin >> a;
+    
+    cout << "Array: ";
+    for(const auto& a: arr) cout << a << " ";
+    cout << "\n";
+    
+    SegmentTree<vector<int>, int> segTree(arr,
+                                        [](const vector<int>& a, const vector<int>& b,
+                                        const int level) -> vector<int> {
+                                            return {min(a[0], b[0]), max(a[1], b[1])};
+                                        },
+                                        {INT_MAX, INT_MIN},
+                                        [](const int& a) -> vector<int> {
+                                            return {a, a};
+                                        });
+    
+    
+    cout << "Operations:\n";
+    cout << "1. getMinMax(L, R)\n";
+    cout << "2. updateValue(idx, newVal)\n\n";
+    while (q--) {
+        char choice; cin >> choice; cin.ignore();
+        
+        if (choice=='1') {
+            int L, R;
+            cin >> L >> R;
+            cout << "Selected: getMinMax(" << L << ", " << R << ")\n";
+            vector<int> res = segTree.query(L, R);
+            cout << res[0] << " " << res[1] << "\n";
+        } else {
+            int idx, newVal;
+            cin >> idx >> newVal;
+            cout << "Selected: updateValue(" << idx << ", " << newVal << ")\n";
+            segTree.update(idx, newVal);
+            cout << "Array: ";
+            for(const auto& a: arr) cout << a << " ";
+            cout << "\n";
+        }
+        cout << "\n";
+    }
+    return 0;
+}
