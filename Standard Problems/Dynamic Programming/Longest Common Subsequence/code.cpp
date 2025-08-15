29064dcb54abcee9698b66a4c4b0672b3c96eeaf
Devansh Seth <129330424+Devansh-Seth-DEV@users.noreply.github.com>
2025-08-16 04:43:26 +0530

    Update code.cpp

--- Standard Problems/Dynamic Programming/Longest Common Subsequence/code.cpp
+++ Standard Problems/Dynamic Programming/Longest Common Subsequence/code.cpp
@@ -12,8 +12,8 @@ class Solution {
         Given 2 strings ω1 and ω2
         where:
             ω ϵ Σ*
-            (ω1)i denotes element at index i in string ω1
-            (ω2)j denotes element at index j in string ω2
+            ω1[i] denotes element at index i in string ω1
+            ω2[j] denotes element at index j in string ω2
             
             |ω| = length of string ω
             i ϵ {1, 2, ..., |ω1|}
@@ -22,21 +22,21 @@ class Solution {
     string w1, w2;
     
     /*
-       ѱ(i, j) - storage function
+       V̄(i, j) - storage function
        stores the computed value of V*(i, j)
        
        defined as:
-            ѱ(i, j) := V*(i, j)
+            V̄(i, j) := V*(i, j)
     */
-    int psi[1001][1001];
+    int V_bar[1001][1001];
     
     /*
         χ(i, j) - Indicator function
-        tells if we've already computed ѱ(i, j)
+        tells if we've already computed V̄(i, j)
         
         defined as:
-            χ(i, j) :=  { 1,   ѱ(i, j) is computed
-                        { 0,    otherwise
+            χ(i, j) :=  { 1,   V̄(i, j) is computed
+                        { 0,   otherwise
         }
     */
     bool chi[1001][1001]; 
@@ -47,7 +47,7 @@ class Solution {
         
         defined as:
             V*(i, j) := { 0,                            i ∉ {1, 2, ..., |ω1|}, j ∉ {1, 2, ...,|ω2|}
-                        { 1 + V*(i-1, j-1),             (ω1)i = (ω2)j
+                        { 1 + V*(i-1, j-1),             ω1[i] = ω2[j]
                         { max(V*(i-1, j), V*(i, j-1)),  otherwise
     */
     int V_star(int i, int j) {
@@ -55,15 +55,15 @@ class Solution {
         if(i<1 || i>w1.length() || j<1 || j>w2.length())
             return 0;
         
-        // χ(i, j) = 1 means ѱ(i, j) is computed so return the stored value
-        if(chi[i][j]) return psi[i][j];
+        // χ(i, j) = 1 means V̄(i, j) is computed so return the stored value
+        if(chi[i][j]) return V_bar[i][j];
         
         chi[i][j] = true;
-        if(w1[i-1] == w2[j-1]) // (ω1)i = (ω2)j
-            return psi[i][j] = 1 + V_star(i-1, j-1);
+        if(w1[i-1] == w2[j-1]) // ω1[i] = ω2[j]
+            return V_bar[i][j] = 1 + V_star(i-1, j-1);
         
         // otherwise
-        return psi[i][j] = max(V_star(i-1, j), V_star(i, j-1));
+        return V_bar[i][j] = max(V_star(i-1, j), V_star(i, j-1));
     }
     
 public:
@@ -72,14 +72,14 @@ public:
         w1 = std::move(s1);
         w2 = std::move(s2);
         
-        int N = w1.length();
-        int M = w2.length();
+        int n = w1.length();
+        int m = w2.length();
         
-        for(int i=0; i<=N; i++)
-            for(int j=0; j<=M; j++)
+        for(int i=0; i<=n; i++)
+            for(int j=0; j<=m; j++)
                 chi[i][j] = false;
         
-        return V_star(N, M);
+        return V_star(n, m);
     }
 };
 
@@ -103,13 +103,13 @@ int main() {
 	Solution sol;
 
 	while(t--) {
-	    string s1, s2;
-	    
-	    read_line(ss);
-	    ss >> s1 >> s2;
-
-	    int len = sol.lcs(s1, s2);
-	    cout << len << "\n";
+        string s1, s2;
+        
+        read_line(ss);
+        ss >> s1 >> s2;
+        
+	    auto res = sol.lcs(s1, s2);
+	    cout << res << "\n";
 	}
 
     return 0;
