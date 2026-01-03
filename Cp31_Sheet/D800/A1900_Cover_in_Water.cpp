c728d088ed9141635cf88982c93f72d22a1173e3
Devansh Seth <129330424+Devansh-Seth-DEV@users.noreply.github.com>
2026-01-03 20:53:02 +0530

    Update A1900_Cover_in_Water.cpp

--- Cp31_Sheet/D800/A1900_Cover_in_Water.cpp
+++ Cp31_Sheet/D800/A1900_Cover_in_Water.cpp
@@ -1,51 +1,70 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-void boostIO() {
-    ios::sync_with_stdio(false);
+/**
+ * @brief Optimizes C++ I/O operations by desyncing with 
+ *        stdio and untying cin from cout.
+ */
+inline void fastIO() {
+    ios_base::sync_with_stdio(false);
     cin.tie(nullptr);
+    cout.tie(nullptr);
 }
 
-class Solution {
+class Solver {
 public:
-    static void runTestCases() {
-        int cases;
-        cin >> cases;
-        
-        while (0 != cases--) {
-            int n;
-            cin >> n;
-            
-            string cells;
-            cin >> cells;
-            
-            int minActions = solve(n, cells);
-            cout << minActions << "\n";
-        }
+    /** @brief Entry point for handling multiple test cases. */
+    static void execute() {
+        int testCases = 1;
+        // Comment this out for single test case problems
+        cin >> testCases;
+        while(testCases--)  solve();
     }
     
-    static int solve(int n, string cells) {
+    
+    /**
+     * @brief Handles the reading of the input unit and printing
+              the result.
+     */
+    static void solve() {
+        // 1. Read input parameters
+        int n;
+        cin >> n;
+        
+        string cells;
+        cin >> cells;
+        
+        // 2. Process logic
+        auto result = process(n, cells);
+        
+        // 3. Output result
+        cout << result << "\n";
+    }
+    
+    
+    /** @brief Core logic for a single test case unit. */
+    static int process(int n, string cells) {
+        // Local variables
+        int result;
         bool is3InARowEmpty = false;
         int emptyCellCount = 0;
         
+        // Implementation Logic Here
         for(int i=0; i<n; i++) {
-            // count total empty cells
-            if ('.' == cells[i]) emptyCellCount++;
-            
-            // check for 3 consecutive cells
+            if (cells[i] == '.') emptyCellCount++;
             if (i>=2 &&
-                '.' == cells[i-2] && 
-                '.' == cells[i-1] && 
-                '.' == cells[i])
-                is3InARowEmpty = true;
+                cells[i-2] == '.' &&
+                cells[i-1] == '.' &&
+                cells[i] == '.') is3InARowEmpty = true;
         }
         
-        return (is3InARowEmpty ? 2 : emptyCellCount);    
+        result = is3InARowEmpty ? 2 : emptyCellCount;
+        return result;
     }
 };
 
 int main() {
-    boostIO();
-    Solution::runTestCases();
+    fastIO();
+    Solver::execute();
     return 0;
 }
