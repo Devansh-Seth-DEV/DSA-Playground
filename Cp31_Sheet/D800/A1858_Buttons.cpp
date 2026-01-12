c90be73274ad74bd91385d987b9a8559985273aa
Devansh-Seth-DEV <dev.devanshseth.engg0072@gmail.com>
2026-01-12 11:31:01 +0530

    new_prob: Added Buttons problem D-800 PN-A1858

---
+++ Cp31_Sheet/D800/A1858_Buttons.cpp
@@ -0,0 +1,100 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+inline void fastIO() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+}
+
+class Printer {
+public:
+    template <typename T>
+    void print(const T& val, const char* end = "\n") {
+        cout << val << end;
+    }
+
+    void print(bool val, const char* end = "\n") {
+        cout << (val ? "YES" : "NO") << end;
+    }
+
+    void print(const char* s, const char* end = "\n") {
+        cout << s << end;
+    }
+
+    void print(const string& s, const char* end = "\n") {
+        cout << s << end;
+    }
+
+    template <typename T>
+    void print(const vector<T>& vec, const char* end = "\n") {
+        for (size_t i = 0; i < vec.size(); ++i)
+            print(vec[i], i + 1 < vec.size() ? " " : "");
+        cout << end;
+    }
+
+    void print(const vector<bool>& vec, const char* end = "\n") {
+        for (size_t i = 0; i < vec.size(); ++i)
+            print(vec[i], i + 1 < vec.size() ? " " : "");
+        cout << end;
+    }
+
+    template <typename T, size_t N>
+    void print(const T (&arr)[N], const char* end = "\n") {
+        for (size_t i = 0; i < N; ++i)
+            print(arr[i], i + 1 < N ? " " : "");
+        cout << end;
+    }
+
+    template <size_t N>
+    void print(const char (&arr)[N], const char* end = "\n") {
+        for (size_t i = 0; i < N; ++i)
+            print(arr[i], i + 1 < N ? " " : "");
+        cout << end;
+    }
+
+    template <typename X, typename Y>
+    void print(const pair<X, Y>& p, const char* end = "\n") {
+        print(p.first, " ");
+        print(p.second, end);
+    }
+};
+
+class Solver {
+private:
+    Printer printer;
+
+public:
+    void execute() {
+        int testCases = 1;
+        cin >> testCases;
+        while (testCases--) solve();
+    }
+
+    void solve() {
+        int a, b, c;
+        cin >> a >> b >> c;
+
+        auto result = process(a, b, c);
+        printer.print(result);
+    }
+
+    string process(int a, int b, int c) {
+        string result;
+
+        bool isNextTurnAnna = (c%2 == 1);
+        
+        if (a==b && isNextTurnAnna) result = "First";
+        else if (a==b && !isNextTurnAnna) result = "Second";
+        else result = (a>b) ? "First" : "Second";
+
+        return result;
+    }
+};
+
+int main() {
+    fastIO();
+    Solver solver;
+    solver.execute();
+    return 0;
+}
